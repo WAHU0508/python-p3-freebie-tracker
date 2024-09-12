d0cfbccdedad8085ef27cd230b9e7968821c05e8
WAHU0508 <sharongikenye@gmail.com>
2024-09-12 20:26:08 +0300

    Company, Dev and Freebie models

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,6 +8,13 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+company_dev = Table(
+    'company_devs',
+    Base.metadata,
+    Column('company_id', ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id', ForeignKey('devs.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -16,14 +23,37 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref=backref('company'))
+    devs = relationship('Dev', secondary=company_dev, back_populates='companies')
+
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f'Company(id={self.id}, ' + \
+            f'name={self.name})'
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
+    
+    freebies = relationship('Freebie', backref=backref('dev'))
+    companies = relationship('Company', secondary=company_dev, back_populates='devs')
 
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f'Dev(id={self.id}, ' + \
+            f'name={self.name})'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    
+    id = Column(Integer, primary_key = True)
+    item_name = Column(String)
+    value = Column(Integer)
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    def __repr__(self):
+        return f'Freebie(id={self.id}, ' + \
+            f'item_name={self.item_name}, ' + \
+            f'value={self.value})'
+    
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python3
+from faker import Faker
+import random
 
-# Script goes here!
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Company, Dev, Freebie
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+
+    fake = Faker()
+    
+    company_1 = Company(name='Google', founding_year=1998)
+    company_2 = Company(name='Apple', founding_year=1976)
+    company_3 = Company(name='Facebook', founding_year=2004)
+    session.add(company_1)
+    session.add(company_2)
+    session.add(company_3)
+    session.commit()
+
+    dev_1 = Dev(name='Alice')
+    dev_2 = Dev(name='Bob')
+    dev_3 = Dev(name='Charli')
+    session.add(dev_1)
+    session.add(dev_2)
+    session.add(dev_3)
+    session.commit()
+
+    freebie_1 = Freebie(item_name = 'T-shirt', value = 15, company_id = company_1.id, dev_id = dev_1.id)
+    freebie_2 = Freebie(item_name = 'Mug', value = 10, company_id = company_2.id, dev_id = dev_2.id)
+    freebie_3 = Freebie(item_name = 'Sticker', value = 5, company_id = company_3.id, dev_id = dev_3.id)
+    session.add(freebie_1)
+    session.add(freebie_2)
+    session.add(freebie_3)
+    session.commit()
